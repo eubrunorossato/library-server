cc681f20617573142a939605af5e43110d699051
Bruno Rossato de Araujo <Bruno.Araujo1@ibm.com>
2021-08-21 00:32:56 -0300

    add .env config

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const app = require('./config/express');
 const https = require('https');
 const fs = require('fs');
 
 https.createServer({
-  key: fs.readFileSync('../certs/server.key'),
-  cert: fs.readFileSync('../certs/server.cert'),
+  key: fs.readFileSync('./certs/server.key'),
+  cert: fs.readFileSync('./certs/server.cert'),
 }, app).listen(process.env.PORT, () => {
   console.log(`Server Started on port ${process.env.PORT}`);
 })
