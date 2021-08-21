63f1134cd6da73188f528a7383b96fbd5b81f247
Bruno Rossato de Araujo <Bruno.Araujo1@ibm.com>
2021-08-21 00:29:17 -0300

    set up inital express structure

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./config/express');
+const https = require('https');
+const fs = require('fs');
+
+https.createServer({
+  key: fs.readFileSync('../certs/server.key'),
+  cert: fs.readFileSync('../certs/server.cert'),
+}, app).listen(process.env.PORT, () => {
+  console.log(`Server Started on port ${process.env.PORT}`);
+})
