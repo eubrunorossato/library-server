f036496fafa667cf13dff0a673a8bd98be5d5a53
Bruno Rossato de Araujo <Bruno.Araujo1@ibm.com>
2021-08-21 00:39:16 -0300

    set up json response and url encoded

--- config/express.js
+++ config/express.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.bodyParser());
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json())
+
 
 module.exports = app;
