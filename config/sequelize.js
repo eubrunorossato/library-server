45f137768a0a26cc621e31a3616980b3d0381207
Bruno Rossato de Araujo <Bruno.Araujo1@ibm.com>
2021-08-23 09:16:24 -0300

    chnage file structure

--- config/sequelize.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS, {
-  host: process.env.DATABASE_HOST,
-  dialect: 'postgres'
-});
-
-module.exports = sequelize;
