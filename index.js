09f465453d56e4de243597e51693eb3d985405fa
Bruno Rossato de Araujo <Bruno.Araujo1@ibm.com>
2021-08-23 09:17:21 -0300

    change file struture

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 require('dotenv').config();
-const app = require('./config/express');
+const app = require('./src/config/express');
 const https = require('https');
 const fs = require('fs');
-const sequelize = require('./config/sequelize');
-try {
-  sequelize.authenticate();
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
 
 https.createServer({
-  key: fs.readFileSync('./certs/server.key'),
-  cert: fs.readFileSync('./certs/server.cert'),
-}, app).listen(process.env.PORT, () => {
-  console.log(`Server Started on port ${process.env.PORT}`);
-})
+    key: fs.readFileSync('./certs/server.key'),
+    cert: fs.readFileSync('./certs/server.cert'),
+  }, app).listen(process.env.PORT, async () => {
+    console.log(`Server Started on port ${process.env.PORT}`);
+});
