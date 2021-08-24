dca811e455725b3b707a2caf8092c857503cd924
Bruno Rossato de Araujo <Bruno.Araujo1@ibm.com>
2021-08-24 01:41:42 -0300

    catch picture in file upload

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const { create } = require('./services/books');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 module.exports = (app) => {
   app.post('/api/books/create', async (req, res, next) => {
-    const response = await create(req.body);
+    const { data } = req.files.book_picture
+    const response = await create(req.body, data);
     res.json(response);
     next();
   });
