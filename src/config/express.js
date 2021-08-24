71f912fe24065c084622745e3d9ef185332bb9ad
Bruno Rossato de Araujo <Bruno.Araujo1@ibm.com>
2021-08-24 01:41:18 -0300

    add express file upload

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const postgres = require('../database/index');
 const routes = require('../routes');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(fileUpload())
 routes(app);
 postgres();
 
