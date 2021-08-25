e79d17f13baa885196b280879026000908a13f78
eubrunorossato <Bruno.Araujo1@ibm.com>
2021-08-25 19:59:24 -0300

    export create connection function and connection

--- src/database/index.js
+++ src/database/index.js
@@ -3,9 +3,13 @@ const Books = require('../models/books');
 const databaseConfig = require('../config/database');
 
 const models = [Books];
+const connection = new Sequelize(databaseConfig);
 
-module.exports = () => {
-  const connection = new Sequelize(databaseConfig)
+const createConnection = () => {
   connection.sync();
   models.map(model => model.init(connection));
+};
+
+module.exports = {
+  createConnection, connection
 }
