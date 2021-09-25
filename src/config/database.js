192f422a99352c8167dd1e1c6a4277db44a5557e
eubrunorossato <Bruno.Araujo1@ibm.com>
2021-09-25 18:53:14 -0300

    change database config to docker

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "username": "brunorossato",
-  "password": null,
-  "database": "library",
+  "username": process.env.DATABASE_USER,
+  "password": process.env.DATABASE_PASS,
+  "database": process.env.DATABASE_NAME,
   "host": "127.0.0.1",
   "dialect": "postgres"
 }
