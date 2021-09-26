07a3dd3c1b760ae80cd0553f3393c68f3aca9433
eubrunorossato <Bruno.Araujo1@ibm.com>
2021-09-25 21:57:49 -0300

    create genres api

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,26 @@
-const { create, getAll } = require('./services/books');
+const books = require('./services/books');
+const genre = require('./services/genre');
 
 module.exports = (app) => {
-  app.post('/api/books/create', async (req, res, next) => {
-    const { data } = req.files.book_picture;
-    const { code, message } = await create(req.body, data);
-
+  app.post('/api/genre/create', async (req, res, next) => {
+    const { code, message } = await genre.create(req.body);
     res.status(code).json(message);
     next();
   });
+  app.get('/api/genre/getAll', async (req, res, next) => {
+    const { code, genres } = await genre.getAll();
+    res.status(code).json(genres);
+    next();
+  });
 
+  app.post('/api/books/create', async (req, res, next) => {
+    const { data } = req.files.book_picture;
+    const { code, message } = await books.create(req.body, data);
+    res.status(code).json(message);
+    next();
+  });
   app.get('/api/books/getAll', async (req, res, next) => {
-    const response = await getAll();
+    const response = await books.getAll();
     res.json(response);
     next();
   });
---
+++ src/services/genre.js
@@ -0,0 +1,44 @@
+const Genre = require('../models/genre');
+const { connection } = require('../database/index');
+const { QueryTypes } = require('sequelize');
+
+const checkValues = (genre) => {
+  if (genre.name === '' || !genre.name || genre.name === null) {
+    throw new Error('Missing required fields');
+  }
+};
+
+module.exports = {
+  create: async (body) => {
+    try {
+      checkValues(body)
+      const { name } = body;
+      const genre = await Genre.create({ name });
+      return {
+        code: 200,
+        message: `Genre ${genre.name} was registered sucessfully`
+      }
+    } catch (error) {
+      return {
+        code: 500,
+        message: error.message
+      }
+    }
+  },
+  getAll: async () => {
+    try {
+      const genres = await connection.query('select * from "Genres"', {
+        type: QueryTypes.SELECT
+      });
+      return {
+        code: 200,
+        genres,
+      }
+    } catch (error) {
+      return {
+        code: 500,
+        message: error.message
+      }
+    }
+  }
+}
